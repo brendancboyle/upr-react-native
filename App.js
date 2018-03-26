82e81c59651bf252ca32884473146ee897612458
Brendan Boyle <brendancboyle@me.com>
2018-03-25 22:37:28 -0400

    Added VSCode debugger support

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, Button, View } from 'react-native';
 
 export default class App extends React.Component {
+
+  testFunction() {
+    console.log("Test")
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Button title="Hello" onPress={this.testFunction} />
       </View>
     );
   }

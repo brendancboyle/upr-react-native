b140151d146f946957736f65d2d57df9e9629e14
Brendan Boyle <brendancboyle@me.com>
2018-04-02 09:35:14 -0400

    Added instruction navigation

--- app/components/Instructions/Instruction1.js
+++ app/components/Instructions/Instruction1.js
@@ -1,29 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
-import Header from './Header';
-import Controls from './Controls';
 import Button from '../General/Button';
-import InfoIcon from './Info.png'
 
 export default class LoginPage extends React.Component {
-  static navigationOptions = ({ navigation }) => {
-    return {
-      title: 'UPR: Remote',
-      headerRight: (
-        <Button
-          onPress={() => navigation.navigate('Instructions')}
-          image={InfoIcon}
-        />
-      )
-    }
+  static navigationOptions = {
+    title: 'Instructions'
   };
 
   render() {
     return (
       <SafeAreaView style={styles.container}>
         <StatusBar barStyle="light-content" />
-        <Header />
-        <Controls />
       </SafeAreaView>
     );
   }
--- app/containers/AppNavigation.js
+++ app/containers/AppNavigation.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 import LoginPage from './LoginPage';
+import Instruction1 from '../components/Instructions/Instruction1';
 
-export default StackNavigator(
+const LoginNavigator = StackNavigator(
   {
     Login: {
       screen: LoginPage
@@ -10,6 +11,34 @@ export default StackNavigator(
   },
   {
     initialRouteName: 'Login',
+    headerMode: 'none'
+  }
+);
+
+const InstructionNavigator = StackNavigator(
+  {
+    Instruction1: {
+      screen: Instruction1
+    }
+  },
+  {
+    initialRouteName: 'Instruction1',
+    headerMode: 'none'
+  }
+);
+
+export default StackNavigator(
+  {
+    Login: {
+      screen: LoginNavigator,
+    },
+    Instructions: {
+      screen: InstructionNavigator
+    }
+  },
+  {
+    initialRouteName: 'Login',
+    mode: 'modal',
     navigationOptions: {
       headerStyle: {
         backgroundColor: '#330033'

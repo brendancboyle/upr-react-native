d76cc7248c237c883d0cc59f256ed298b3436da4
Brendan Boyle <brendancboyle@me.com>
2019-05-24 01:40:44 -0400

    Renamed bundle ID

--- upr/android/app/src/main/java/com/dbztech/universalpresenterremote/upr/MainActivity.java
+++ upr/android/app/src/main/java/com/dbztech/universalpresenterremote/upr/MainActivity.java
@@ -1,4 +1,4 @@
-package com.upr;
+package com.dbztech.universalpresenterremote.upr;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
--- upr/android/app/src/main/java/com/dbztech/universalpresenterremote/upr/MainApplication.java
+++ upr/android/app/src/main/java/com/dbztech/universalpresenterremote/upr/MainApplication.java
@@ -1,4 +1,4 @@
-package com.upr;
+package com.dbztech.universalpresenterremote.upr;
 
 import android.app.Application;
 

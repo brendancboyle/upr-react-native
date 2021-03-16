60486f5385dc504bca213d4fb33d9d6690da623c
Brendan Boyle <brendan.boyle@epicgames.com>
2021-03-16 19:09:01 -0400

    Fix URL path

--- upr/app/libraries/UPRKit/index.js
+++ upr/app/libraries/UPRKit/index.js
@@ -1,6 +1,6 @@
 import App from './App';
 import Session from './Session';
 
-export const API_ENDPOINT = 'https://universalpresenterremote.com/';
+export const API_ENDPOINT = 'https://universalpresenterremote.com';
 
 export default { App, Session };
